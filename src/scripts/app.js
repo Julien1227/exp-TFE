1a318c371e5cdbd5936f439033ccd21d778698f4
Julien <julien19991227@gmail.com>
2021-03-11 12:01:54 +0100

    Version clean de l'expérimentation - Le son s'espompe plus la couleur est sombre, blanche, ou déstaurée !

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,22 +2,40 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext;
 var context = new AudioContext();
 var myBuffer;
 
-var colorSlider = document.querySelector('#colorValue');
-var satSlider = document.querySelector('#satValue');
-var lumSlider = document.querySelector('#lumValue');
+var input = document.querySelectorAll('#input');
+var p = document.querySelectorAll('#inputP');
 
+var inputs = [];
+var ps = [];
 
-//Récupère la valeur Couleur du slider
-var colorValue = document.querySelector('#colorValue').value/10;
-console.log(colorValue);
+var count = 0;
 
-//Récupère la valeur Stauration du slider
-var satValue = document.querySelector('#satValue').value/100;
-console.log(colorValue);
+//Récupère les inputs
+input.forEach((input) => {
+  inputs[count] = input;
+  count++;
+});
+count = 0;
 
-//Récupère la valeur Luminiosité du slider
-var lumValue = document.querySelector('#lumValue').value/100;
-console.log(colorValue);
+//Récupère les P
+p.forEach((p) => {
+  ps[count] = p;
+  count++;
+});
+
+console.log(inputs);
+console.log(ps);
+
+//Déclare les p et innerHTLM les valeurs des inputs
+
+var color = document.querySelector('.color');
+
+for (let i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('input', function () {
+    p[i].innerHTML = inputs[i].value;
+    color.style.backgroundColor = "hsl("+inputs[0].value+", "+inputs[1].value+"%, "+inputs[2].value+"%)"
+    }, false);
+};
 
 
 var request = new XMLHttpRequest();
@@ -37,25 +55,29 @@ request.onload = function() {
 request.send();
 
 function playSound(buffer) {
-
-  //Redéclare mes variables sat lum et color
-  colorValue = document.querySelector('#colorValue').value;
-  console.log(colorValue);
-
-  satValue = document.querySelector('#satValue').value;
-  console.log(satValue);
-
-  lumValue = document.querySelector('#lumValue').value;
-  console.log(lumValue);
-
-  //Crée un buffer sur lequel appliquer des effets (je pense)
   var source = context.createBufferSource(), g = context.createGain();
   source.buffer = buffer;
   source.start(0);
-  if(lumValue < satValue) {
-    g.gain.value = lumValue/100;
-  }else if(lumValue > satValue){
+
+  //Récupère les valeurs des sliders
+  var lumValue = inputs[2].value;
+  var satValue = inputs[1].value;
+
+  //Si la couleur est lumineuse, alors le son s'estompe également
+  if(lumValue > 50) {
+    var lumValue = 100 - lumValue;
+    console.log(lumValue);
+  }else{
+    lumValue = lumValue;
+  }
+
+  //Prends la valeur la plus basse et l'utilise pour le gain
+  if(satValue < lumValue){
+    console.log(satValue);
     g.gain.value = satValue/100;
+  }else if(lumValue < satValue){
+    console.log(lumValue);
+    g.gain.value = lumValue/100;
   }
 
   //Connecte la source
@@ -63,52 +85,9 @@ function playSound(buffer) {
   g.connect(context.destination);
 }
 
-function clickHandler(e) {;
+function clickHandler(e) {
     playSound(myBuffer);
 }
-
-var colorP = document.querySelector('#colorP');
-var satP = document.querySelector('#satP');
-var lumP = document.querySelector('#lumP');
-colorP.innerHTML = colorValue;
-satP.innerHTML = satValue;
-lumP.innerHTML = lumValue;
-
-var color = document.querySelector('.color');
-
-colorSlider.addEventListener('input', function () {
-    //Redéclare mes variables sat lum et color
-    colorValue = document.querySelector('#colorValue').value;
-    satValue = document.querySelector('#satValue').value;
-    lumValue = document.querySelector('#lumValue').value;
-
-    colorP.innerHTML = colorValue;
-    satP.innerHTML = satValue;
-    lumP.innerHTML = lumValue;
-    color.style.backgroundColor = "hsl("+ colorValue +", "+ satValue +"%, "+ lumValue +"%)";
-}, false);
-
-satSlider.addEventListener('input', function () {
-    //Redéclare mes variables sat lum et color
-    colorValue = document.querySelector('#colorValue').value;
-    satValue = document.querySelector('#satValue').value;
-    lumValue = document.querySelector('#lumValue').value;
-
-    colorP.innerHTML = colorValue;
-    satP.innerHTML = satValue;
-    lumP.innerHTML = lumValue;
-    color.style.backgroundColor = "hsl("+ colorValue +", "+ satValue +"%, "+ lumValue +"%)";}, false);
-
-lumSlider.addEventListener('input', function () {
-    //Redéclare mes variables sat lum et color
-    colorValue = document.querySelector('#colorValue').value;
-    satValue = document.querySelector('#satValue').value;
-    lumValue = document.querySelector('#lumValue').value;
-
-    colorP.innerHTML = colorValue;
-    satP.innerHTML = satValue;
-    lumP.innerHTML = lumValue;
-    color.style.backgroundColor = "hsl("+ colorValue +", "+ satValue +"%, "+ lumValue +"%)";}, false);
 
 clickme = document.getElementById('clickme');
 clickme.addEventListener('click',clickHandler);
