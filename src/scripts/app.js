bbae39bd700cb70abae2524efdc5eac5158963e5
Julien <julien19991227@gmail.com>
2021-03-13 10:30:41 +0100

    Ajout de la gestion du tempo par la noirseur/blancheur de la couleur. A CHANGER, le pitch varie avec le tempo.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,6 +4,8 @@ var myBuffer;
 
 var input = document.querySelectorAll('#input');
 var p = document.querySelectorAll('#inputP');
+var clickstart = document.getElementById('clickstart');
+var clickstop = document.getElementById('clickstop');
 
 //Récupère les inputs
 var inputs = [];
@@ -68,19 +70,19 @@ request.onload = function() {
 }
 request.send();
 
+
 function playSound(buffer) {
   declareValues();
 
   //Crée ma source et un effet "gain"
   var source = context.createBufferSource();
   var g = context.createGain();
+  
 
   //Si la couleur est lumineuse, alors le son s'estompe également
   if(lumValue > 50) {
     lumValue = 100 - lumValue;
     console.log(lumValue);
-  }else{
-    lumValue = lumValue;
   }
 
   //Prends la valeur la plus basse et l'utilise pour le gain
@@ -92,17 +94,28 @@ function playSound(buffer) {
     g.gain.value = lumValue/100;
   }
 
+  //Prends en compte la luminiosité car le tempo doit correspondre au taux de noirceur de la couleur
+  if(inputs[2].value < 50) {
+    source.playbackRate.value = 0.5 + lumValue/100;
+  }else{
+    source.playbackRate.value = 0.5 + satValue/100;
+  }
+
   source.buffer = buffer;
   source.start(0);
+  //source.loop = true;
 
   //Connecte la source
   source.connect(g);
   g.connect(context.destination);
+
+  clickstop.addEventListener('click', e => {
+    source.stop(0);
+  });
 }
 
 function clickHandler(e) {
     playSound(myBuffer);
 }
 
-clickme = document.getElementById('clickme');
-clickme.addEventListener('click',clickHandler);
+clickstart.addEventListener('click',clickHandler);
