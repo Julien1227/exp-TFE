693d1257e43a6fa0c4ae1520b79a84870ead75ae
Julien <julien19991227@gmail.com>
2021-03-16 16:02:06 +0100

    3e fonctionnalité

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,10 @@
+//Web audio api
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 var context = new AudioContext();
 var myBuffer;
+
+var request = new XMLHttpRequest();
+
 
 var input = document.querySelectorAll('#input');
 var p = document.querySelectorAll('#inputP');
@@ -45,31 +49,55 @@ function declareValues() {
 //Lorsqu'on déplace le slider, récupère la bonne value, la modifie dans le span comme dans la couleur
 var color = document.querySelector('.color');
 for (let i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener('input', function () {
+  inputs[i].addEventListener('input', (e) => {
     declareValues();
-
+    
     p[i].innerHTML = inputs[i].value;
     color.style.backgroundColor = "hsl("+colorValue+", "+satValue+"%, "+lumValue+"%)"
-    }, false);
+    
+    //Si il s'agit du slider de couleur
+    let target = e.currentTarget;
+    if(target.hasAttribute('data-input', 'color') == true) {
+      //Détection de la teinte et chargement du bon son
+      if(colorValue < 35 ) {
+        console.log('rouge');
+        request.open('GET', './assets/mp3/cello.mp3', true);
+      }else if(colorValue >= 35 && colorValue <= 45 ) {
+        console.log('orange');
+        request.open('GET', './assets/mp3/singing.mp3', true);
+      }else if(colorValue > 45 && colorValue <= 60 ) {
+        console.log('jaune');
+        request.open('GET', './assets/mp3/trumpet.mp3', true);
+      }else if(colorValue > 60 && colorValue <= 145 ) {
+        console.log('vert');
+        request.open('GET', './assets/mp3/basoon.mp3', true);
+      }else if(colorValue > 145 && colorValue <= 260 ) {
+        console.log('bleu');
+        request.open('GET', './assets/mp3/tuba.mp3', true);
+      }else if(colorValue > 260 && colorValue <= 300 ) {
+        console.log('mauve');
+        request.open('GET', './assets/mp3/tuba.mp3', true);
+      }else if(colorValue > 300 && colorValue <= 335 ) {
+        console.log('rose');
+        request.open('GET', './assets/mp3/guitare.mp3', true);
+      }else if(colorValue > 335 && colorValue <= 360 ) {
+        console.log('rouge');
+        request.open('GET', './assets/mp3/cello.mp3', true);
+      }
+    }
+    request.responseType = 'arraybuffer';
+    
+    // Decode asynchronously ???
+    request.onload = function() {
+      context.decodeAudioData(request.response, function(theBuffer) {
+        myBuffer = theBuffer;
+        console.log(theBuffer);
+      });
+    }
+    request.send();
+    
+  }, false);
 };
-
-
-var request = new XMLHttpRequest();
-
-//Chargement du son
-request.open('GET', './assets/mp3/guitare.mp3', true);
-console.log('Request done');
-request.responseType = 'arraybuffer';
-
-// Decode asynchronously ???
-request.onload = function() {
-  context.decodeAudioData(request.response, function(theBuffer) {
-    myBuffer = theBuffer;
-    console.log(theBuffer);
-  });
-}
-request.send();
-
 
 function playSound(buffer) {
   declareValues();
@@ -77,7 +105,6 @@ function playSound(buffer) {
   //Crée ma source et un effet "gain"
   var source = context.createBufferSource();
   var g = context.createGain();
-  
 
   //Si la couleur est lumineuse, alors le son s'estompe également
   if(lumValue >= 50) {
